7e8e2d7765ec9186ed183c1da6d2ce5dfda79db2
Ankit Ahirrao <ahirraoankit5@gmail.com>
2022-06-29 01:33:37 +0530

    destory book feature add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,23 +15,30 @@ class BooksController < ApplicationController
   end
 
   def edit 
+    @book = current_user.books.find(params[:id])
   end
 
   def create
     @book = current_user.books.build(book_params)
     if @book.save 
-      redirect_to books_url
+      redirect_to books_url, notice: "Book is added"
     else
       render 'new'
     end
   end
 
   def update 
+    @book = current_user.books.find(params[:id])
+    if @book.update(book_params)
+      redirect_to books_url, notice: "Book details are updated"
+    else
+      render 'edit'
+    end
   end
 
-  def destory
-    @book.destory
-    redirect_to books_url
+  def destroy
+    current_user.books.find(params[:id]).destroy
+    redirect_to books_url, notice: "Book is destroyed"
   end
 
   def check_admin 
