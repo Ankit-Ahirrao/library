09670771319ca23f128a63724521c9e849840470
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-06-30 12:28:23 +0530

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
   resources :books
 
 end
