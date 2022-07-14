38e5dd9a453ef2b0765d1038987b0f7ec94464bd
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-07-14 17:39:24 +0530

    Add seed data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  host = 'polar-beyond-69423.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => "polar-beyond-69423.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
