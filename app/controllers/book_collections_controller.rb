d71491c1c9aec455f6b244d51e48add73710d22a
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-07-18 18:11:52 +0530

    auth add

--- app/controllers/book_collections_controller.rb
+++ app/controllers/book_collections_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class BookCollectionsController < ApplicationController
+  load_and_authorize_resource
   def index
     @search = RequestSearch.new(params[:search])
     @book_collections = @search.get_request
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,35 +5,12 @@ class Ability
   include ApplicationHelper
 
   def initialize(user)
-    can :read, Book
-
-    can :manage, Book if user.is_admin 
-    can :read, BookCollection if user.is_admin
-    can :issue_book_request, Book if !user.is_admin
-    can :cancel_book_request, Book if !user.is_admin
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    if user.is_admin
+      can :manage, :all
+    else 
+      can :read, Book
+      can :issue_book_request, Book
+      can :cancel_book_request, Book 
+    end
   end
 end
